bafdb5470e82fdb2aa52413865a4be0e9c922e93
Ellie H <elliehealy20@outlook.com>
2024-02-29 20:44:35 +0000

    Add JS code and live buttons, add footer

---
+++ script.js
@@ -0,0 +1,31 @@
+const codeBtn2 = document.getElementById("codeBtn2");
+codeBtn2.addEventListener("click", function () {
+    window.location.href = "https://github.com/e-healy/frontend-mentor-challenges/tree/main/blog-preview-card-main"
+});
+
+const liveBtn2 = document.getElementById("liveBtn2");
+liveBtn2.addEventListener("click", function () {
+    window.location.href = "./blog-preview-card-main/index.html"
+});
+
+
+const codeBtn3 = document.getElementById("codeBtn3");
+codeBtn3.addEventListener("click", function () {
+    window.location.href = "https://github.com/e-healy/frontend-mentor-challenges/tree/main/faq-accordion-main"
+});
+
+const liveBtn3 = document.getElementById("liveBtn3");
+liveBtn3.addEventListener("click", function () {
+    window.location.href = "./faq-accordion-main/index.html"
+});
+
+
+const codeBtn4 = document.getElementById("codeBtn4");
+codeBtn4.addEventListener("click", function () {
+    window.location.href = "https://github.com/e-healy/frontend-mentor-challenges/tree/main/order-summary-component-main"
+});
+
+const liveBtn4 = document.getElementById("liveBtn4");
+liveBtn4.addEventListener("click", function () {
+    window.location.href = "./order-summary-component-main/index.html"
+});
